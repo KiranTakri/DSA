79f7246a02091031b1a1208841a10fc124cf0435
Kiran Takri <kirantakri18@gmail.com>
2026-02-28 11:25:43 +0530

    Fourth commit

---
+++ PracticeArray/AlternativeAddingOne.java
@@ -0,0 +1,10 @@
+package PracticeArray;
+
+import java.util.Vector;
+
+public class AlternativeAddingOne {
+    public static void main(String[] args) {
+        Vector<Integer> data=new Vector<>();
+
+    }
+}
