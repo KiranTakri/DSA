8c486b44fb6971b1bdc36ce4edd3d84987a6475a
Kiran Takri <kirantakri18@gmail.com>
2026-02-28 09:45:16 +0530

    commit

---
+++ PracticeArray/AddingOne.java
@@ -0,0 +1,14 @@
+package PracticeArray;
+
+import java.util.Vector;
+
+public class AddingOne {
+    public static void main(String[] args) {
+        Vector<Integer> list=new Vector<>();
+        list.add(2);
+        list.add(4);
+        list.add(6);
+        list.add(8);
+        System.out.println(list);
+    }
+}
