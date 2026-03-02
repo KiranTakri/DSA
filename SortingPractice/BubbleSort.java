657c2a54235544494a49bfe1cd9c91381c076307
Kiran Takri <kirantakri18@gmail.com>
2026-03-02 21:26:13 +0530

    Initial commit

---
+++ SortingPractice/BubbleSort.java
@@ -0,0 +1,28 @@
+package SortingPractice;
+
+public class BubbleSort {
+    public static void print(int[] arr){
+        for(int ele:arr){
+            System.out.print(ele+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr={98,2,-3,65,74,3,2,0,-67};
+        int[] arr2=sortingArray(arr);
+        print(arr2);
+    }
+    public static int[] sortingArray(int[] arr){
+        int n= arr.length;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if(arr[i]>arr[j]){
+                    int swap=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=swap;
+            }
+        }
+
+        }
+        return arr;
+    }
+}
