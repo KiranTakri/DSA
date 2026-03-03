8d0c290f718e864d53da254a26ce0f532cd03236
Kiran Takri <kirantakri18@gmail.com>
2026-03-03 19:42:00 +0530

    Updated Code

--- SortingPractice/SelectionSort.java
+++ SortingPractice/SelectionSort.java
@@ -14,7 +14,7 @@ public class SelectionSort {
     public static int[] sortingArray(int[] arr){
         int n= arr.length;
         for (int i = 0; i < arr.length; i++) {
-            for (int j = i; j < arr.length; j++) {
+            for (int j = i+1; j < arr.length; j++) {
                 if(arr[i]>arr[j]){
                     int swap=arr[i];
                     arr[i]=arr[j];
